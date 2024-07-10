7b5edfa32f375dd0c2a491a7276b5cabc59aa071
21mulfi <mulfi.indra@widyatama.ac.id>
2024-07-10 19:10:27 +0700

    styling dashboard admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,44 @@
 @include('template.master')
+<style>
+  .dashboard-card {
+      padding: 20px;
+      color: white;
+      border-radius: 5px;
+  }
+  .card-title {
+      font-size: 20px;
+      font-weight: bold;
+  }
+  .card-number {
+      font-size: 30px;
+      font-weight: bold;
+  }
+  .bg-guru {
+    background-color: #FDE49E;
+  }
+  .bg-user {
+    background-color: #DD761C;
+  }
+  .bg-kelas {
+    background-color: #FEB941;
+  }
+  .bg-siswa {
+    background-color: #B7B597;
+  }
+  .bg-ortu {
+    background-color: #36BA98;
+  }
+  .card-icon {
+      font-size: 3rem;
+      margin-bottom: 10px;
+    }
 
+  .shadow {
+    box-shadow: -2px 0px 30px 0px rgba(0,0,0,1);
+    -webkit-box-shadow: -2px 0px 30px 0px rgba(0,0,0,1);
+    -moz-box-shadow: -2px 0px 30px 0px rgba(0,0,0,1);
+  }
+</style>
 <body class="bg-light">
   {{-- NAVBAR --}}
   @include('template.dashnavbar')
@@ -18,42 +57,98 @@
         @endif</h5>
         <hr>
     <div class="container my-3">
+      @if(Auth::user()->role == 'admin')
       <div class="row">
-        @if(Auth::user()->role == 'admin')
-        <table>
-          <tr>
-            <td width="40%"><p class="poppins-regular">User Terdaftar</p></td>
-            <td><p class="poppins-regular">:</p></td>
-            <td><p class="poppins-regular">{{ $userCount }} User</p></td>
-          </tr>
-
-          <tr>
-            <td><p class="poppins-regular">Kelas Terdaftar</p></td>
-            <td><p class="poppins-regular">:</p></td>
-            <td><p class="poppins-regular">{{ $classCount }} Kelas</p></td>
-          </tr>
-
-          <tr>
-            <td><p class="poppins-regular">Guru Terdaftar</p></td>
-            <td><p class="poppins-regular">:</p></td>
-            <td><p class="poppins-regular">{{ $teacherCount }} Guru</p></td>
-          </tr>
-          
-          <tr>
-            <td><p class="poppins-regular">Siswa Terdaftar</p></td>
-            <td><p class="poppins-regular">:</p></td>
-            <td><p class="poppins-regular">{{ $studentCount }} Siswa</p></td>
-          </tr>
-          
-          <tr>
-            <td><p class="poppins-regular">Orang Tua Siswa Terdaftar</p></td>
-            <td><p class="poppins-regular">:</p></td>
-            <td><p class="poppins-regular">{{ $parentCount }} Orang Tua Siswa</p></td>
-          </tr>
-        </table>
-        @endif
-      </div>   
+        <div class="col-md-6 mb-1">
+            <div class="dashboard-card bg-user shadow">
+                <div class="row">
+                    <div class="col-8 text-dark">
+                        <div class="card-title">User Terdaftar</div>
+                        <div class="card-number" id="userCount"></div>
+                    </div>
+                    <div class="col-4 text-right mt-3">
+                      <div class="card-icon"><img class="img-fluid d-none d-xl-block" width="50" src="{{ asset('assets/user.png') }}" alt="User"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 mb-1">
+            <div class="dashboard-card bg-siswa">
+                <div class="row">
+                    <div class="col-8 text-dark">
+                        <div class="card-title text-dark">Siswa Terdaftar</div>
+                        <div class="card-number" id="classCount"></div>
+                    </div>
+                    <div class="col-4 text-right mt-3">
+                      <div class="card-icon"><img class="img-fluid d-none d-xl-block" width="50" src="{{ asset('assets/siswa.png') }}" alt="User"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 mb-1">
+            <div class="dashboard-card bg-guru shadow">
+                <div class="row">
+                    <div class="col-8 text-dark">
+                        <div class="card-title">Guru Terdaftar</div>
+                        <div class="card-number" id="studentCount"></div>
+                    </div>
+                    <div class="col-4 text-right mt-3">
+                      <div class="card-icon"><img class="img-fluid d-none d-xl-block" width="50" src="{{ asset('assets/guru.png') }}" alt="User"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 mb-1">
+            <div class="dashboard-card bg-kelas shadow">
+                <div class="row">
+                    <div class="col-8 text-dark">
+                        <div class="card-title">Kelas Terdaftar</div>
+                        <div class="card-number" id="teacherCount"></div>
+                    </div>
+                    <div class="col-4 text-right mt-3">
+                      <div class="card-icon"><img class="img-fluid d-none d-xl-block" width="50" src="{{ asset('assets/kelas.png') }}" alt="User"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12 mb-1">
+            <div class="dashboard-card bg-ortu shadow">
+                <div class="row">
+                    <div class="col-8 text-dark">
+                        <div class="card-title">Orang Tua Terdaftar</div>
+                        <div class="card-number mb-4" id="parentCount"></div>
+                    </div>
+                    <div class="col-4 text-right mt-3">
+                      <div class="card-icon"><img class="img-fluid d-none d-xl-block" width="50" src="{{ asset('assets/ortu.png') }}" alt="User"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
     </div>
   </div>
+  <script>
+    const counters = {
+            userCount: {{ $userCount }},
+            classCount: {{ $classCount }},
+            parentCount: {{ $parentCount }},
+            studentCount: {{ $studentCount }},
+            teacherCount: {{ $teacherCount }}
+        };
 
+        function animateCounter(id, value) {
+            let count = 0;
+            const interval = setInterval(() => {
+                document.getElementById(id).innerHTML = ++count;
+                if (count === value) {
+                    clearInterval(interval);
+                }
+            }, 100);
+        }
+
+        for (const [id, value] of Object.entries(counters)) {
+            animateCounter(id, value);
+        }
+</script>
 @include('template.endmaster')
--- resources/views/pages/admin/registration.blade.php
+++ resources/views/pages/admin/registration.blade.php
@@ -4,7 +4,7 @@
   @include('template.dashnavbar')
   
   <div class="content bg-white container-sm border my-5 rounded px-5 py-3 pb-5">
-    <h5 class="text-center">Verifikasi Registrasi</h5>
+    <h5 class="text-center fw-bold poppins-regular">Verifikasi Registrasi</h5>
     <hr>
     <div class="row align-items-start">
       <div class="table-responsive">
@@ -60,112 +60,6 @@
           @endforeach
           </tbody>
         </table>
-      </div>
-    </div>
-  </div>
-
-  <div class="modal fade" id="detailRegistrasi" tabindex="-1" aria-labelledby="detailRegistrasiLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header text-light" style="background-color: #1B96CE">
-          <h1 class="modal-title fs-5" id="detailRegistrasiLabel">Detail Registrasi</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <div class="row bg-secondary">
-            <p class="fw-bold h5 text-light mt-2 mb-2">Data Siswa</p>
-          </div>
-            <div class="mb-3 mt-3">
-                <label for="name" class="form-label fw-bold">Nama Lengkap</label>
-                <p>Ujang</p>
-            </div>
-            <div class="mb-3">
-              <label for="tempat_lahir" class="form-label fw-bold">Tempat, Tanggal Lahir</label>
-              <p>Bandung, 25 Februari 2021</p>
-          </div>
-          <div class="mb-3">
-              <label for="tanggal_lahir" class="form-label fw-bold">Jenis Kelamin</label>
-              <p>Laki-laki</p>
-          </div>
-          <div class="mb-3">
-            <label for="alamat" class="form-label fw-bold">Alamat</label>
-            <p>Indonesia</p>
-          </div>
-          <!-- <div class="mb-3">
-            <label for="tanggal_lahir" class="form-label fw-bold">Bahasa Di Rumah</label>
-            <p>Indonesia</p>
-          </div> -->
-          <div class="mb-3">
-            <label for="agama" class="form-label fw-bold">Agama</label>
-            <p>Islam</p>
-          </div>
-          <!-- <div class="mb-3">
-            <label for="tanggal_lahir" class="form-label fw-bold">Pas Foto</label>
-            <button class="btn btn-primary"><a class="text-decoration-none text-light" href="#" target="_blank">Lihat Foto</a></button>
-          </div> -->
-          <div class="row bg-secondary">
-            <p class="fw-bold h5 text-light mt-2 mb-2">Data Ayah</p>
-          </div>
-          <div class="mb-3 mt-3">
-            <label for="name" class="form-label fw-bold">Nama Lengkap</label>
-            <p>Asep</p>
-          </div>
-          <!-- <div class="mb-3">
-            <label for="name" class="form-label fw-bold">Alamat</label>
-            <p>Jl. Garut No. 13 Bandung</p>
-          </div> -->
-          <!-- <div class="mb-3">
-            <label for="name" class="form-label fw-bold">Email</label>
-            <p>asep@gmail.com</p>
-          </div> -->
-          <div class="mb-3">
-            <label for="name" class="form-label fw-bold">No. Telepon / WhatsApp</label>
-            <p>0892310391023</p>
-          </div>
-          <!-- <div class="mb-3">
-            <label for="name" class="form-label fw-bold">Aktif WhatsApp?</label>
-            <p>Ya</p>
-          </div> -->
-          <div class="row bg-secondary">
-            <p class="fw-bold h5 text-light mt-2 mb-2">Data Ibu</p>
-          </div>
-          <div class="mb-3 mt-3">
-            <label for="name" class="form-label fw-bold">Nama Lengkap</label>
-            <p>Mayang</p>
-          </div>
-          <!-- <div class="mb-3">
-            <label for="name" class="form-label fw-bold">Alamat</label>
-            <p>Jl. Garut No. 13 Bandung</p>
-          </div> -->
-          <!-- <div class="mb-3">
-            <label for="name" class="form-label fw-bold">Email</label>
-            <p>mayang@gmail.com</p>
-          </div> -->
-          <div class="mb-3">
-            <label for="name" class="form-label fw-bold">No. Telepon / WhatsApp</label>
-            <p>0892310391024</p>
-          </div>
-          <!-- <div class="mb-3">
-            <label for="name" class="form-label fw-bold">Aktif WhatsApp?</label>
-            <p>Tidak</p>
-          </div> -->
-          <div class="row bg-secondary">
-            <p class="fw-bold h5 text-light mt-2 mb-2">Informasi Pendaftaran</p>
-          </div>
-          <div class="mb-3 mt-3">
-            <label for="name" class="form-label fw-bold">Sumber Informasi</label>
-            <p>Teman Kerja</p>
-            <p>Search Engine Google</p>
-          </div><div class="mb-3">
-            <label for="name" class="form-label fw-bold">Referensi Masuk</label>
-            <p>Maman - 08923130129301</p>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <p class="text-left fw-bold">Verifikasi Registrasi</p>
-          <button class="btn btn-success" title="Setuju"><i class="fa fa-check" aria-hidden="true"></i></button>
-            <button class="btn btn-danger" title="Tolak"><i class="fa fa-times" aria-hidden="true"></i></button>
-        </div>
       </div>
     </div>
   </div>
